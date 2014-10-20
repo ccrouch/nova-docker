11eb99fb7e202c12fb252387ceb52bc578af953e
ccrouch <charles.crouch@gmail.com>
2014-10-19 20:26:23 -0500

    stop trying to restart the service
    when running devstack+nova-docker driver inside a container I'm hitting
    '/var/lib/docker/devicemapper': Permission denied
    when doing 
    sudo /usr/sbin/service docker restart
    so lets try not doing the restart

--- contrib/devstack/extras.d/70-docker.sh
+++ contrib/devstack/extras.d/70-docker.sh
@@ -49,7 +49,7 @@ if [[ $VIRT_DRIVER == "docker" ]]; then
     fi
 
     # Start the daemon - restart just in case the package ever auto-starts...
-    restart_service docker
+    #restart_service docker
 
     echo "Waiting for docker daemon to start..."
     DOCKER_GROUP=$(groups | cut -d' ' -f1)
